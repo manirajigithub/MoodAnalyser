58c322aa92b3a8bdd229b6ecf28fc59f1552a5a7
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-08-22 17:38:03 +0530

    [Rajeswari]Added The Handle Exception

---
+++ MoodAnalyse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC2
+{
+    public class MoodAnalyse
+    {
+
+        public string Info;
+        public MoodAnalyse(string info)
+        {
+            this.Info = info;
+        }
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (Info.Contains("sad", StringComparison.OrdinalIgnoreCase))
+                {
+                    return "sad";
+                }
+                else
+                {
+
+                    return "happy";
+                }
+            }
+            catch
+            {
+                return "happy";
+            }
+
+
+        }
+    }
+}
+}
