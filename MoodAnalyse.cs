fd0d0cb106b32d3a6c8ea87e9ce320e5a4917679
mani1 <mani1@NANI143>
2022-08-23 14:32:31 +0530

    [Rajeswari]Handle The Custom Exception

---
+++ MoodAnalyse.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC4
+{
+    public class MoodAnalyse
+    {
+        public string Info;
+        public MoodAnalyse(string info)
+        {
+            this.Info = info;
+        }
+        public string AnalyseEmptyMood()
+        {
+            try
+            {
+                if (this.Info.Equals(string.Empty))
+                {
+                    throw new Custom_Exception(Custom_Exception.Mood.Empty, "Its Empty");
+                }
+            }
+
+
+            catch (NullReferenceException)
+            {
+                throw new Custom_Exception(Custom_Exception.Mood.Empty, "Its Empty");
+            }
+            catch (Custom_Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ex.Message;
+            }
+            return null;
+        }
+        public string AnalyseNullMood()
+        {
+
+
+            try
+            {
+                if (this.Info == null)
+                {
+                    throw new Custom_Exception(Custom_Exception.Mood.Null, "Its Null");
+                }
+            }
+
+
+            catch (NullReferenceException)
+            {
+                throw new Custom_Exception(Custom_Exception.Mood.Null, "Its Null");
+            }
+            catch (Custom_Exception ex1)
+            {
+                Console.WriteLine(ex1.Message);
+                return ex1.Message;
+            }
+            return null;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (Info.Contains("sad", StringComparison.OrdinalIgnoreCase))
+                {
+                    return "sad";
+                }
+                else
+                {
+
+                    return "happy";
+                }
+            }
+            catch
+            {
+                return "happy";
+            }
+        }
+    }
+}
