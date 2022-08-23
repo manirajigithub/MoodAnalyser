f38beecc7466dbdc9175df0994b89f11131dbeab
mani1 <mani1@NANI143>
2022-08-23 14:28:40 +0530

    [Rajeswari]Added MoodAnalyser

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using MoodAnalyserUC4;
+namespace MoodAnalyserUC4
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {          
+
+            Console.WriteLine("Welcome to Mood Analyser program");
+
+
+            MoodAnalyser check = new MoodAnalyser("msg");
+            Console.WriteLine(check.AnalyseMood("I am sad"));
+            Console.WriteLine(check.AnalyseMood("I am happy"));
+
+
+            MoodAnalyser check1 = new MoodAnalyser("msg");
+            check1.AnalyseAnyMood("I am in any mood");
+        }
+    }
+}
+
+
+
+
+
+
