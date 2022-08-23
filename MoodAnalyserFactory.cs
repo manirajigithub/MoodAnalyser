a713f2ef8ffd13d8c4ff8ee18dab6d6442da2eed
manirajigithub <109149403+manirajigithub@users.noreply.github.com>
2022-08-23 14:39:16 +0530

    [Rajeswari]Added MoodAnalyser Factory

---
+++ MoodAnalyserFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyserUC4
+{
+    public class MoodAnalyserFactory
+    {
+
+        public object Mood_Analyser(string Class, string Constructor)
+        {
+            string c = @"." + Constructor + "$";
+            Match result = Regex.Match(Class, c);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    Type AnalyserType = assembly.GetType(Class);
+                    var res = Activator.CreateInstance(AnalyserType);
+                    return res;
+                }
+                catch (Exception ex)
+                {
+                    throw new Custom_Exception(Custom_Exception.ExceptionType.NO_SUCH_CLASS, "Can't Found This Class ");
+                }
+            }
+
+            throw new Custom_Exception(Custom_Exception.ExceptionType.NO_SUCH_CONSTRUCTOR, "Can't Found This Constructor ");
+
+        }
+
+    }
+}
