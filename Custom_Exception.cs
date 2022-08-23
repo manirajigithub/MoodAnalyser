fa6ef66ce77fb7e41c1288ef864d9f105ecdbcd0
mani1 <mani1@NANI143>
2022-08-23 14:53:26 +0530

    [Rajeswari]using Reflection And check The Constructor Exception

---
+++ Custom_Exception.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUC5
+{
+    public class Custom_Exception : Exception
+    {
+
+        public enum Mood
+        {
+            Empty,
+            Null
+        }
+
+        public readonly Mood Minfo;
+
+        public Custom_Exception(Mood minfo, string info) : base(info)
+        {
+            this.Minfo = minfo;
+        }
+
+
+        /// <summary>
+        /// Used For Reflection
+        /// </summary>
+        ExceptionType type;
+        public enum ExceptionType
+        {
+            NULL, EMPTY, NO_SUCH_CLASS, NO_SUCH_CONSTRUCTOR
+        }
+        public Custom_Exception(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
